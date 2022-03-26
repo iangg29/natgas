48dd3589be15a20a9a33779a48519570f87a4395
MandukoGR <gurom84@outlook.com>
2022-03-26 17:59:25 -0600

    Implemented status cards

--- client/src/pages/Test.tsx
+++ client/src/pages/Test.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import CardSolicitud from '../components/Cards/CardSolicitud'
+import CardMiSolicitud from '../components/Cards/CardMiSolicitud'
 const Test = () : JSX.Element => {
   return (
-    <CardSolicitud name = "Armando Gutiérrez" department= "Recursos Humanos" date = "10 Enero 2022" turn = "Primera parte del día"/>
+    <CardMiSolicitud name = "Armando Gutiérrez" department= "Recursos Humanos" date = "10 Enero 2022" turn = "Primera parte del día" state = "Aprobado"/>
   )
 }
 
