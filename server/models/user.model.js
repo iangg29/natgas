eaeaa2c27817298cccd5380b47b745f3f42a850a
Ian García <iangarciag@outlook.com>
2022-03-29 19:59:10 -0600

    Modified user model.

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -13,7 +13,7 @@ module.exports = class extends Base {
     }
 
     async save() {
-        const numero = await db
+        const number = await db
             .insert({
                 name: this.name,
                 lastname: this.lastname,
@@ -21,7 +21,7 @@ module.exports = class extends Base {
             })
             .into(this.tableName);
         return db.select('*').from(this.tableName).where({
-            numero,
+            number,
         });
     }
 };
