fcd5b2ef030c4df60adf961f01663d82d4fc9f46
Ian García <iangarciag@outlook.com>
2022-04-01 00:07:32 -0600

    User employment hotfix.

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,11 +1,13 @@
 const base = require('./base.controller');
 const User = require('../models/user.model');
 const UserEmployment = require('../models/views/useremployment.view.model');
+const { getAll } = require('./base.controller');
 
 exports.getUsers = base.getAll(User);
 exports.getUserByEmail = base.getOne(User, 'email');
 exports.getUserByEmployeeNumber = base.getOne(User, 'number');
-exports.getUserEmployment = base.getOne(UserEmployment, 'number');
+exports.getUserEmployment = base.getOne(UserEmployment, 'email');
+exports.getUsersEmployment = base.getAll(UserEmployment);
 exports.createUser = base.createOne(User);
 exports.updateUser = base.updateOne(User, 'number');
 exports.deleteUser = base.deleteOne(User, 'number');
