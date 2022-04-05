e4a1250eef9e89e6bfa0790df9f02415789956b3
MandukoGR <gurom84@outlook.com>
2022-04-05 16:31:59 -0500

    Blog page with pagination and data from database

--- client/src/pages/blog/Blog.tsx
+++ client/src/pages/blog/Blog.tsx
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, {useState, useRef, useEffect} from 'react'
+import axios from 'axios'
 import BlogCard from '../../components/Cards/BlogCard'
 import Title from '../../components/Title/Title'
 import { FaPlusCircle } from 'react-icons/fa'
-const Blog = () => {
+import Pagination from '../../components/Inputs/Pagination'
+const Blog = () : JSX.Element => {
+
+  const [getBlogs, setBlogs] = useState<any[]>([]);
+  const [getPage, setPage] = useState<any>(1);
+  const limit = 15;
+  const topRef = useRef<any>(null);
+
+ useEffect(() => {
+    (async () => {
+        try{
+            const [blogs] = await Promise.all([axios.get(`/blog?sort=idBlogPost&limit=${limit}&page=${getPage}`)]);
+            setBlogs(blogs.data.data.documents);
+
+        }catch (error: any) {
+            alert(error.message);
+        }
+    })();
+  }, [getPage]);
   return (
       <>
         <Title title="Natgas Blog"/>
-        <div className = "grid w-full justify-items-center pt-4 mb-8">
+        <div className = "grid w-full justify-items-center pt-4 mb-8" ref={topRef}>
             <button className = " text-natgas-azul font-bold rounded-full w-[260px] h-[50px] border-natgas-azul-claro border-[5px]">
                 <div className='inline-flex'>
                     <p className = "mt-0.5">Agregar Natgas Blog</p>
@@ -16,14 +35,28 @@ const Blog = () => {
         </div>
         
         <div className='grid  gap-5 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3'>
-            <BlogCard image = "https://scontent.fqro1-1.fna.fbcdn.net/v/t1.6435-9/162478430_1838218953002496_26093472607830674_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=730e14&_nc_ohc=LcoISjRUJZMAX_gckq4&_nc_ht=scontent.fqro1-1.fna&oh=00_AT907TprSGR80RM9wqfGDKjqkhWHOGBrBJX2jA1sdO2XBQ&oe=62707A69"/>
-            <BlogCard image = "https://scontent.fqro1-1.fna.fbcdn.net/v/t1.6435-9/162478430_1838218953002496_26093472607830674_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=730e14&_nc_ohc=LcoISjRUJZMAX_gckq4&_nc_ht=scontent.fqro1-1.fna&oh=00_AT907TprSGR80RM9wqfGDKjqkhWHOGBrBJX2jA1sdO2XBQ&oe=62707A69"/>
-            <BlogCard image = "https://scontent.fqro1-1.fna.fbcdn.net/v/t1.6435-9/162478430_1838218953002496_26093472607830674_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=730e14&_nc_ohc=LcoISjRUJZMAX_gckq4&_nc_ht=scontent.fqro1-1.fna&oh=00_AT907TprSGR80RM9wqfGDKjqkhWHOGBrBJX2jA1sdO2XBQ&oe=62707A69"/>
-            <BlogCard image = "https://scontent.fqro1-1.fna.fbcdn.net/v/t1.6435-9/162478430_1838218953002496_26093472607830674_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=730e14&_nc_ohc=LcoISjRUJZMAX_gckq4&_nc_ht=scontent.fqro1-1.fna&oh=00_AT907TprSGR80RM9wqfGDKjqkhWHOGBrBJX2jA1sdO2XBQ&oe=62707A69"/>
-            <BlogCard image = "https://scontent.fqro1-1.fna.fbcdn.net/v/t1.6435-9/162478430_1838218953002496_26093472607830674_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=730e14&_nc_ohc=LcoISjRUJZMAX_gckq4&_nc_ht=scontent.fqro1-1.fna&oh=00_AT907TprSGR80RM9wqfGDKjqkhWHOGBrBJX2jA1sdO2XBQ&oe=62707A69"/>
-            <BlogCard image = "https://scontent.fqro1-1.fna.fbcdn.net/v/t1.6435-9/162478430_1838218953002496_26093472607830674_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=730e14&_nc_ohc=LcoISjRUJZMAX_gckq4&_nc_ht=scontent.fqro1-1.fna&oh=00_AT907TprSGR80RM9wqfGDKjqkhWHOGBrBJX2jA1sdO2XBQ&oe=62707A69"/>
+            {getBlogs.length > 0 ? (
+                getBlogs.map((blog) => (
+                <BlogCard
+                    image = {blog.image}
+                    title={blog.title}
+                    content = {blog.content}
+                    date={new Date(blog.date).toLocaleDateString()}
+                />
+                ))
+            ) : (
+                <p>No existen blogs</p>
+            )}
 
         </div>
+        <Pagination
+            length={getBlogs.length}
+            getPage={getPage}
+            setPage={setPage}
+            reference={topRef}
+            limit = {limit}
+        />
+  
 
         
         
