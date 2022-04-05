c0f19fe9af766e711ed1cb2fc59a43166170d73d
CesarJimenezVilleda02 <65324745+CesarJimenezVilleda02@users.noreply.github.com>
2022-04-05 08:13:18 -0500

    initial backend commit to reports

--- server/controllers/report.controller.js
+++ server/controllers/report.controller.js
@@ -1,8 +1,10 @@
 const base = require('./base.controller');
 const Report = require('../models/report.model');
+const Row = require('../models/row.model');
 
 exports.getReports = base.getAll(Report);
 exports.getReport = base.getOne(Report, 'idReporte');
 exports.createReport = base.createOne(Report);
 exports.updateReport = base.updateOne(Report, 'idReporte');
 exports.deleteReport = base.deleteOne(Report, 'idReporte');
+exports.getRowsFromReport = base.getOne(Row, 'idReporte');
