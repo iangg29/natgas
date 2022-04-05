2e17336f1990b1d3f3f3228664d902bee0bb3286
Pantro Huerta <pantro12@live.com.mx>
2022-04-05 13:18:33 -0500

    Se agregó el componente Background2

--- client/src/components/Background/Background.tsx
+++ client/src/components/Background/Background.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 
 const Background = () => {
   return (
-    
     <div className='relative matrix w-full h-72 bg-gradient-to-r from-[#00A99D] to-[#098B90]'> </div>
   )
 }
--- client/src/pages/TestPage.tsx
+++ client/src/pages/TestPage.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Background from "../components/Background/Background";
+import Background2 from "../components/Background/Background2";
 import TitleWhite from "../components/Title/TitleWhite";
 
 const TestPage = (): JSX.Element => {
@@ -7,7 +8,10 @@ const TestPage = (): JSX.Element => {
     <div>
       <TitleWhite title="Ejemplo" />
       <div>
-      <Background/>
+        <Background/>
+      </div>
+      <div>
+        <Background2/>
       </div>
     </div>
   );
