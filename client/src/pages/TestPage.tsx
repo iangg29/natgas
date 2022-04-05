8a9ce6666f30f2560679ea26d9cdb99fb9385907
Pantro Huerta <pantro12@live.com.mx>
2022-04-05 13:12:34 -0500

    Se realizó el componente Background

--- client/src/pages/TestPage.tsx
+++ client/src/pages/TestPage.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import Background from "../components/Background/Background";
 import TitleWhite from "../components/Title/TitleWhite";
 
 const TestPage = (): JSX.Element => {
   return (
     <div>
       <TitleWhite title="Ejemplo" />
+      <div>
+      <Background/>
+      </div>
     </div>
   );
 };
