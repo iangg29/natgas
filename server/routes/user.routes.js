93b5146ee35d0aaf1ee5216dc7a7289357708c59
CesarJimenezVilleda02 <65324745+CesarJimenezVilleda02@users.noreply.github.com>
2022-04-04 23:59:06 -0500

    Bug with user routes fixed

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -3,14 +3,16 @@ const userController = require('../controllers/user.controller');
 
 const router = express.Router();
 
+router.route('/email/:id').get(userController.getUserByEmail);
+router.route('/employment').get(userController.getAllUserEmploymentDetails);
+router
+    .route('/employment/:id')
+    .get(userController.getOneUsersEmploymentDetails);
 router.route('/').get(userController.getUsers).post(userController.createUser);
 router
     .route('/:id')
     .get(userController.getUserByEmployeeNumber)
     .patch(userController.updateUser)
     .delete(userController.deleteUser);
-router.route('/email/:id').get(userController.getUserByEmail);
-router.route('/employment').get(userController.getUsersEmployment);
-router.route('/employment/:id').get(userController.getUserEmployment);
 
 module.exports = router;
