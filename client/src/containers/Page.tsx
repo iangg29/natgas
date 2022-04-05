a289e5da9edd8bc09b920f2f7cfd69b750b8d8d0
CesarJimenezVilleda02 <65324745+CesarJimenezVilleda02@users.noreply.github.com>
2022-04-05 10:47:16 -0500

    Initial setup for reports pages

--- client/src/containers/Page.tsx
+++ client/src/containers/Page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Helmet } from "react-helmet";
+import Title from "../components/Title/Title";
 
 type Props = {
   title: string;
@@ -13,9 +14,7 @@ const Page = ({ children, headTitle, title }: Props): JSX.Element => {
       <Helmet>
         <title>{headTitle} | NatGas</title>
       </Helmet>
-      <h1 className="text-center text-2xl font-bold font-bold text-natgas-azul dark:text-gray-50 md:text-left">
-        {title}
-      </h1>
+      <Title title={title} />
       <div className="py-5">{children}</div>
     </div>
   );
---
+++ client/src/pages/reports/AllReports.tsx
@@ -0,0 +1,33 @@
+import Page from "../../containers/Page";
+import React from "react";
+import axios from "axios";
+
+const Reports = (): JSX.Element => {
+  const [getReports, setReports] = React.useState<any[]>([]);
+
+  React.useEffect(() => {
+    (async () => {
+      try {
+        const departments = await axios.get("/report");
+        console.log(departments);
+        setReports(departments.data.data.documents);
+      } catch (error: any) {
+        alert(error.response.message);
+      }
+    })();
+  }, []);
+
+  return (
+    <>
+      <Page title="Reportes" headTitle="Reportes">
+        <div>
+          {getReports.map((dpt: any) => (
+            <div>{dpt.name}</div>
+          ))}
+        </div>
+      </Page>
+    </>
+  );
+};
+
+export default Reports;
