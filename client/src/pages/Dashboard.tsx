c2035d8668b0c307003f9b58fe53e2586b9f1975
Alan Razo <a01703350@itesm.mx>
2022-04-06 18:24:36 -0500

    Terminar cards de blogs

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -3,6 +3,8 @@ import { useAuth0 } from "@auth0/auth0-react";
 import Page from "../containers/Page";
 import ButtonBar from "../components/ButtonBar/ButtonBar";
 import { Link } from "react-router-dom";
+import CardBlogs from "../components/Cards/CardBlogs";
+import TitleWhite from "../components/Title/TitleWhite";
 
 const Dashboard = (): JSX.Element => {
   const { user } = useAuth0();
@@ -12,6 +14,12 @@ const Dashboard = (): JSX.Element => {
       <div className="py-10">
         <Link to="/app/requests">Requests</Link>
         <ButtonBar />
+        <TitleWhite title="Últimos Natgas Blogs" />
+        <CardBlogs 
+          title={'Embajadores Natgas'}
+          description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc, amet nibh sed eu aliquam volutpat..."} 
+          date={"6/4/22"} 
+        />
       </div>
     </Page>
   );
