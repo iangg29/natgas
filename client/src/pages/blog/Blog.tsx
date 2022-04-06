6752a10083570c751bafbef4f169adc37b764841
Pablo Cesar Jimenez Villeda <pablocesarjimenezvilleda@gmail.com>
2022-04-06 17:23:09 -0500

    Search blogs added

--- client/src/pages/blog/Blog.tsx
+++ client/src/pages/blog/Blog.tsx
@@ -1,71 +1,74 @@
-import React, {useState, useRef, useEffect} from 'react'
-import axios from 'axios'
-import BlogCard from '../../components/Cards/BlogCard'
-import Title from '../../components/Title/Title'
-import { FaPlusCircle } from 'react-icons/fa'
-import Pagination from '../../components/Inputs/Pagination'
-const Blog = () : JSX.Element => {
-
+import React, { useState, useRef, useEffect } from "react";
+import axios from "axios";
+import BlogCard from "../../components/Cards/BlogCard";
+import Title from "../../components/Title/Title";
+import InputLong from "../../components/Inputs/InputLong";
+import { FaPlusCircle } from "react-icons/fa";
+import Pagination from "../../components/Inputs/Pagination";
+const Blog = (): JSX.Element => {
   const [getBlogs, setBlogs] = useState<any[]>([]);
   const [getPage, setPage] = useState<any>(1);
+  const [getTitle, setTitle] = useState<string>("");
   const limit = 15;
   const topRef = useRef<any>(null);
 
- useEffect(() => {
+  useEffect(() => {
     (async () => {
-        try{
-            const [blogs] = await Promise.all([axios.get(`/blog?sort=idBlogPost&limit=${limit}&page=${getPage}`)]);
-            setBlogs(blogs.data.data.documents);
-
-        }catch (error: any) {
-            alert(error.message);
-        }
+      try {
+        const [blogs] = await Promise.all([
+          axios.get(
+            `/blog?title_like=${getTitle}&sort=idBlogPost&limit=${limit}&page=${getPage}`,
+          ),
+        ]);
+        setBlogs(blogs.data.data.documents);
+      } catch (error: any) {
+        alert(error.message);
+      }
     })();
-  }, [getPage]);
+  }, [getPage, getTitle]);
   return (
-      <>
-        <Title title="Natgas Blog"/>
-        <div className = "grid w-full justify-items-center pt-4 mb-8" ref={topRef}>
-            <button className = " text-natgas-azul font-bold rounded-full w-[260px] h-[50px] border-natgas-azul-claro border-[5px]">
-                <div className='inline-flex'>
-                    <p className = "mt-0.5">Agregar Natgas Blog</p>
-                    <FaPlusCircle className='text-3xl ml-4 text-natgas-verde' /> 
-                </div>
-            </button>
-        </div>
-        
-        <div className='grid  gap-5 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3'>
-            {getBlogs.length > 0 ? (
-                getBlogs.map((blog) => (
-                <BlogCard
-                    image = {blog.image}
-                    title={blog.title}
-                    content = {blog.content}
-                    date={new Date(blog.date).toLocaleDateString()}
-                />
-                ))
-            ) : (
-                <p>No existen blogs</p>
-            )}
-
-        </div>
-        <Pagination
-            length={getBlogs.length}
-            getPage={getPage}
-            setPage={setPage}
-            reference={topRef}
-            limit = {limit}
+    <>
+      <Title title="Natgas Blog" />
+      <div className="my-5">
+        <InputLong
+          label="Buscar blog"
+          getVal={getTitle}
+          setVal={setTitle}
+          placeholder="Buscar..."
         />
-  
+      </div>
+      <div className="mb-8 grid w-full justify-items-center pt-4" ref={topRef}>
+        <button className=" h-[50px] w-[260px] rounded-full border-[5px] border-natgas-azul-claro font-bold text-natgas-azul">
+          <div className="inline-flex">
+            <p className="mt-0.5">Agregar Natgas Blog</p>
+            <FaPlusCircle className="ml-4 text-3xl text-natgas-verde" />
+          </div>
+        </button>
+      </div>
 
-        
-        
-      
-      
-      </>
+      <div className="grid  gap-5 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3">
+        {getBlogs.length > 0 ? (
+          getBlogs.map((blog) => (
+            <BlogCard
+              image={blog.image}
+              title={blog.title}
+              content={blog.content}
+              date={new Date(blog.date).toLocaleDateString()}
+            />
+          ))
+        ) : (
+          <p>No existen blogs</p>
+        )}
+      </div>
+      <Pagination
+        length={getBlogs.length}
+        getPage={getPage}
+        setPage={setPage}
+        reference={topRef}
+        limit={limit}
+      />
+    </>
+  );
+};
 
-    
-  )
-}
-
-export default Blog
+export default Blog;
