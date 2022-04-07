c59b52917bf010249fb82375623ef5c2c482d37c
MandukoGR <gurom84@outlook.com>
2022-04-07 13:19:57 -0500

    Duplicated Card blogs removed

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -3,7 +3,6 @@ import { useAuth0 } from "@auth0/auth0-react";
 import Page from "../containers/Page";
 import ButtonBar from "../components/ButtonBar/ButtonBar";
 import { Link } from "react-router-dom";
-import CardBlogs from "../components/Cards/CardBlogs";
 import TitleWhite from "../components/Title/TitleWhite";
 
 const Dashboard = (): JSX.Element => {
@@ -18,11 +17,7 @@ const Dashboard = (): JSX.Element => {
         </div>
         <ButtonBar />
         <TitleWhite title="Últimos Natgas Blogs" />
-        <CardBlogs 
-          title={'Embajadores Natgas'}
-          description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc, amet nibh sed eu aliquam volutpat..."} 
-          date={"6/4/22"} 
-        />
+       
       </div>
     </Page>
   );
