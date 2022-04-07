a1ebde61be4bd5034bcb9e513b31284871f0641b
MandukoGR <gurom84@outlook.com>
2022-04-07 11:43:02 -0500

    Link to add blog page

--- client/src/pages/blog/Blog.tsx
+++ client/src/pages/blog/Blog.tsx
@@ -5,6 +5,7 @@ import Title from "../../components/Title/Title";
 import InputLong from "../../components/Inputs/InputLong";
 import { FaPlusCircle } from "react-icons/fa";
 import Pagination from "../../components/Inputs/Pagination";
+import { Link } from "react-router-dom";
 const Blog = (): JSX.Element => {
   const [getBlogs, setBlogs] = useState<any[]>([]);
   const [getPage, setPage] = useState<any>(1);
@@ -39,12 +40,12 @@ const Blog = (): JSX.Element => {
         />
       </div>
       <div className="mb-8 grid w-full justify-items-center pt-4" ref={topRef}>
-        <button className=" h-[50px] w-[260px] rounded-full border-[5px] border-natgas-azul-claro font-bold text-natgas-azul">
+        <Link to="/app/blog/form" className=" h-[50px] w-[260px] rounded-full border-[5px] border-natgas-azul-claro font-bold text-natgas-azul hover:bg-natgas-azul-claro">
           <div className="inline-flex">
-            <p className="mt-0.5">Agregar Natgas Blog</p>
-            <FaPlusCircle className="ml-4 text-3xl text-natgas-verde" />
+            <p className="mt-2 ml-4">Agregar Natgas Blog</p>
+            <FaPlusCircle className="ml-4 mt-1.5 text-3xl text-natgas-verde" />
           </div>
-        </button>
+        </Link>
       </div>
 
       <div className="grid  gap-5 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3">
