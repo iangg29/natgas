909920421b95c9dd0b559a6a1f5b9baeb5aa660e
Alan Razo <a01703350@itesm.mx>
2022-04-08 09:27:52 -0500

    SSe termino carousel

--- client/src/components/Background/Background.tsx
+++ client/src/components/Background/Background.tsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 const Background = () => {
   return (
-    <div className='relative matrix w-full h-72 bg-gradient-to-r from-[#00A99D] to-[#098B90]'> </div>
+    <div className='relative matrix w-full h-full bg-gradient-to-r from-[#00A99D] to-[#098B90]'>
+    </div>
   )
 }
 
--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -3,8 +3,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 import Page from "../containers/Page";
 import ButtonBar from "../components/ButtonBar/ButtonBar";
 import { Link } from "react-router-dom";
-import CardBlogs from "../components/Cards/CardBlogs";
-import TitleWhite from "../components/Title/TitleWhite";
+import DashCarousel from "../components/Carousel/DashCarousel";
 
 const Dashboard = (): JSX.Element => {
   const { user } = useAuth0();
@@ -12,14 +11,10 @@ const Dashboard = (): JSX.Element => {
   return (
     <Page title={`Bienvenid@ ${user?.name}`} headTitle="Dashboard">
       <div className="py-10">
+        <DashCarousel/>
         <Link to="/app/requests">Requests</Link>
         <ButtonBar />
-        <TitleWhite title="Últimos Natgas Blogs" />
-        <CardBlogs 
-          title={'Embajadores Natgas'}
-          description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc, amet nibh sed eu aliquam volutpat..."} 
-          date={"6/4/22"} 
-        />
+        
       </div>
     </Page>
   );
