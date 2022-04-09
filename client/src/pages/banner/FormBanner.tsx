a23a6a7c998baf1e85542a611bd0843c558d8965
AriannFernando <A01703556@itesm.mx>
2022-04-09 01:38:13 -0500

    Banner frontend connected with backend

--- client/src/pages/banner/FormBanner.tsx
+++ client/src/pages/banner/FormBanner.tsx
@@ -4,12 +4,33 @@ import InputLong from '../../components/Inputs/InputLong';
 import InputP from '../../components/Inputs/InputP';
 import UploadDocument from '../../components/Inputs/UploadDocument';
 import Title from '../../components/Title/Title';
+import axios from 'axios';
 
 const FormBanner = () => {
     const [getTitleB, setTitleB] = useState<any>();
     const [selectedFileB, setSelectedFileB] = useState<any>();
     const [previewB, setPreviewB] = useState<any>();
+
+    const uploadB = async (e:any) => {
+        try{
+            e.preventDefault();
+            const form = new FormData();
+            form.append("name", getTitleB);
+            form.append("date", new Date().toLocaleDateString());
+            form.append("news_photo", selectedFileB);
+            console.log(selectedFileB[0])
   
+            const res = await axios({
+                method: 'POST',
+                url: '/banner',
+                data: form,
+            });
+  
+        }catch (error : any) { 
+            alert(error.response.message);
+            console.log(error.response);
+        }
+    }
     useEffect(() => {
         if(!selectedFileB){
             setPreviewB(undefined)
@@ -20,15 +41,17 @@ const FormBanner = () => {
         return () => URL.revokeObjectURL(objectUrl)
     }, [selectedFileB])
     const onSelectFile = (e : any) => {
-      if (!e.target.files || e.target.files.length === 0) {
-          setSelectedFileB(undefined)
-          return
-      }
-      setSelectedFileB(e.target.files[0])
-    }   
+        if (!e.target.files || e.target.files.length === 0) {
+    
+            setSelectedFileB(undefined)
+            return
+        }
+        setSelectedFileB(e.target.files[0])
+        console.log(e.target.files[0])
+      } 
+
     return (
       <>
-          
           <div className= "grid md:grid-cols-2  sm:grid-cols-1 gap-20">
               <InputLong label="Título" placeholder="Título" getVal = {getTitleB} setVal = {setTitleB}/>
               <UploadDocument label = "Cargar Anuncio" onchange = {onSelectFile}/>
@@ -38,17 +61,8 @@ const FormBanner = () => {
               <img src={previewB} className = "mt-10  md:h-[500px] md:w-[500px]  sm:h-[250px] sm:w-[250px] rounded-md object-cover"/>
           </div>
           <div className = "grid justify-center">
-                <button className = "mt-4  primary-button-blue">Subir</button>
+                <button onClick = {uploadB} className = "mt-4  primary-button-blue">Subir</button>
             </div>
-         
-          
-        
-         
-         
-         
-              
-        
-      
       </>
     )
   }
