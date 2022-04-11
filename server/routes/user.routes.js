a6796d25d3ec078325a12d96efaab97fa7315cd6
Pablo Cesar Jimenez Villeda <65324745+CesarJimenezVilleda02@users.noreply.github.com>
2022-04-11 12:06:23 -0500

    Secured secret columns, makeFieldsPrivate middleware created

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const userController = require('../controllers/user.controller');
-
+const makeFieldsPrivate = require('../utils/privateFields');
+const User = require('../models/user.model');
 const router = express.Router();
+
+router.use(makeFieldsPrivate(User, 'password', 'created_at', 'updated_at'));
 
 router.route('/email/:id').get(userController.getUserByEmail);
 router.route('/employment').get(userController.getAllUserEmploymentDetails);
@@ -9,7 +12,8 @@ router
     .route('/employment/:id')
     .get(userController.getOneUsersEmploymentDetails);
 router.route('/').get(userController.getUsers).post(userController.createUser);
-router.route('/:id')
+router
+    .route('/:id')
     .get(userController.getUserByEmployeeNumber)
     .patch(userController.updateUser)
     .delete(userController.deleteUser);
--- server/utils/privateFields.js
+++ server/utils/privateFields.js
@@ -1,5 +1,22 @@
-const makeFieldsPrivate = (table, ...fields) => {
-    return (req, res, next) => {
+const db = require('./../db/database');
+
+/**
+ * Remove fields from query string or makes them inaccessible.
+ * @constructor
+ * @param {string} Model - The Model of the table to restrict.
+ * @param {array} fields - The fields to be excluded.
+ */
+const makeFieldsPrivate = (Model, ...fields) => {
+    return async (req, res, next) => {
+        if (req.query.fields) {
+            let queryFields = req.query.fields.split(',');
+            queryFields = queryFields.filter((qf) => !fields.includes(qf));
+            req.query.fields = queryFields.join(',');
+        } else {
+            const columnInfo = await db(Model.table).columnInfo();
+            fields.forEach((field) => delete columnInfo[field]);
+            req.query.fields = Object.keys(columnInfo).join(',');
+        }
         next();
     };
 };
