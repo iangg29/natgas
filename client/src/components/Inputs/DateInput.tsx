049c083dbc9044449264e7cfadd08d1fa856f6ef
MandukoGR <gurom84@outlook.com>
2022-04-13 14:12:44 -0500

    Asuetos terminado

--- client/src/components/Inputs/DateInput.tsx
+++ client/src/components/Inputs/DateInput.tsx
@@ -2,14 +2,20 @@ import React from "react";
 
 type Props = {
   label: string; //Escribir de inicio o de fin
+  getVal: any;
+  setVal: any;
 };
 
-const DateInput = ({ label }: Props): JSX.Element => {
+const DateInput = ({ label, getVal, setVal }: Props): JSX.Element => {
   return (
     <>
       <label>
         <div className="mb-2 text-sm font-bold text-gray-700">{label}</div>
-        <input className="input-general" type="date" />
+        <input 
+          onChange={(e) => setVal(e.target.value)}
+          value={getVal} 
+          className="input-general md:w-80" 
+          type="date" />
       </label>
     </>
   );
