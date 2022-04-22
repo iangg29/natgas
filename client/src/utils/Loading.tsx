cf5027e94df1594d0788f7b64198cf2c61e61d42
Ian García <iangarciag@outlook.com>
2022-04-22 00:10:21 -0500

    Minor changes.

--- client/src/utils/Loading.tsx
+++ client/src/utils/Loading.tsx
@@ -1,9 +1,6 @@
 import React from "react";
-import logo from "../assets/img/isotipo-contorno.png";
 
 const Loading = (): JSX.Element => {
-  // TODO: Implement a nice loader with company logo.
-
   return (
     <div className="flex h-screen w-full justify-center p-6 text-lg font-medium text-gray-600 dark:bg-gray-900 dark:text-gray-400">
       <h1>Loading...</h1>
