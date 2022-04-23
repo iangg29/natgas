0550ba65a1f7efcc18c3fe00886f3fe6892f5c4d
Ian García <iangarciag@outlook.com>
2022-04-23 13:46:36 -0500

    Removed unused Auth0 env variables.

--- client/src/react-app-env.d.ts
+++ client/src/react-app-env.d.ts
@@ -2,14 +2,7 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      REACT_APP_AUTH0_DOMAIN: string;
-      REACT_APP_AUTH0_CLIENT_ID: string;
-      REACT_APP_AUTH0_CLIENT_SECRET: string;
-      REACT_APP_DOMAIN: string;
       REACT_APP_API_URI: string;
-
-      REACT_APP_AUTH0_TEST_USERNAME: string;
-      REACT_APP_AUTH0_TEST_PASSWORD: string;
     }
   }
 }
