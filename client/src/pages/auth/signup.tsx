9c5c0f79a56aef47a0a138ebf74d2b75e617b6b0
Alan Razo <a01703350@itesm.mx>
2022-04-24 17:16:58 -0500

    terminar signup

---
+++ client/src/pages/auth/signup.tsx
@@ -0,0 +1,134 @@
+import React from "react";
+import logo from "../../assets/img/Natgas-OFICIAL.png";
+import { Link, useNavigate } from "react-router-dom";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { Helmet } from "react-helmet";
+import { connect } from "react-redux";
+
+type Inputs = {
+    name: string;
+    lastname: string;
+    email: string;
+    password: string;
+    passwordConfirm: string;
+  };
+
+  const Signup = (props: any): JSX.Element => {
+    const { login } = props;
+    const year = new Date().getFullYear();
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: { errors },
+    } = useForm<Inputs>();
+      
+    const navigate = useNavigate();
+      
+    const onSubmit: SubmitHandler<Inputs> = (data: any): void => {
+        login(data, navigate);
+    };
+
+    return (
+        <>
+            <Helmet>
+                <title>NatGas | Sign Up</title>
+            </Helmet>
+            <div className="flex h-screen w-full flex-row">
+                <div className="flex w-full flex-col justify-around md:w-1/2">
+                    <div className="mx-auto w-1/3">
+                        <Link to="/">
+                        <img src={logo} alt="Natgas" />
+                        </Link>
+                    </div>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <div className="mx-auto flex w-11/12 flex-col justify-around rounded-xl text-center">
+                            <h2 className="font-bebas-bold text-2xl">Crea una cuenta</h2>
+                                <div className="my-5 mx-auto flex w-3/4 flex-col space-y-2 md:w-1/2">
+                                <label
+                                    htmlFor="text"
+                                    className="text-left font-gilroy-light"
+                                 >
+                                    Ingresa tu nombre:
+                                </label>
+                                <input
+                                    type="email"
+                                    id="emailInput"
+                                    className="rounded-lg border-0 bg-gray-200 focus:outline-0"
+                                />
+                                <label
+                                    htmlFor="emailInput"
+                                    className="text-left font-gilroy-light"
+                                 >
+                                    Ingresa tu correo electrónico:
+                                </label>
+                                <input
+                                    type="email"
+                                    id="emailInput"
+                                    className="rounded-lg border-0 bg-gray-200 focus:outline-0"
+                                    {...register("email", {
+                                        maxLength: 50,
+                                        required: true,
+                                    })}
+                                />
+                                {errors.email?.type === "required" && (
+                                    <span className="text-xs text-red-700">
+                                        Debes ingresar el correo electrónico.
+                                    </span>
+                                )}
+                                {errors.email?.type === "maxLength" && (
+                                    <span className="text-xs text-red-700">
+                                        El correo electrónico no puede superar los 50 caracteres.
+                                    </span>
+                                )}
+                                <label
+                                    htmlFor="passwordInput"
+                                    className="text-left font-gilroy-light"
+                                >
+                                    Ingresa tu apellido
+                                </label>
+                                <input
+                                    type="password"
+                                    id="passwordInput"
+                                    className="rounded-lg border-0 bg-gray-200 focus:outline-0"
+                                    {...register("password", {
+                                        required: true,
+                                        minLength: 8,
+                                    })}
+                                />
+                                    {errors.password?.type === "minLength" && (
+                                    <span className="text-xs text-red-700">
+                                        La contraseña debe de tener al menos 8 caracteres.
+                                    </span>
+                                    )}
+                                    {errors.password?.type === "required" && (
+                                    <span className="text-xs text-red-700">
+                                        Debes ingresar la contraseña.
+                                    </span>
+                                    )}
+                                </div>
+                                <div className="flex flex-col space-y-5">
+                                    <button
+                                    type="submit"
+                                    className="mx-auto mt-5 w-3/4 rounded-xl border border-natgas-azul bg-natgas-azul px-10 py-1 font-semibold text-white shadow-lg transition-all hover:bg-transparent hover:text-natgas-azul md:w-1/2"
+                                    >
+                                    Iniciar sesión
+                                    </button>
+                                    <Link
+                                    to="/password/recover"
+                                    className="mx-auto font-quicksand-regular text-sm text-natgas-azul hover:underline"
+                                    >
+                                    Olvidé mi contraseña
+                                    </Link>
+                                </div>
+                        </div>
+                    </form> 
+                </div>
+                <div className="login-splash hidden w-full md:block md:w-1/2"></div>
+            </div>
+            
+        </>
+    );
+  };
+
+export default Signup;
