9d006c6abed40107945fb8080bc41449aa596865
MandukoGR <gurom84@outlook.com>
2022-04-27 18:24:24 -0500

    Minor fixes @CesarJimenezVilleda02

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -2,16 +2,18 @@ const express = require('express');
 const userController = require('../controllers/user.controller');
 const { makeFieldsPrivate, restricBody } = require('../utils/protection');
 const User = require('../models/user.model');
+const abacController = require('../controllers/abac.controller')
 const router = express.Router();
 
 router.route('/employment').get(userController.getAllUserEmploymentDetails);
 router
     .route('/employment/:id')
     .get(userController.getOneUsersEmploymentDetails);
-router.use(makeFieldsPrivate(User, 'password', 'created_at', 'updated_at'));
+router.use(abacController.limitRole('HR'), makeFieldsPrivate(User, 'password', 'created_at', 'updated_at'));
 router.route('/').get(userController.getUsers).post(userController.createUser);
 router
     .route('/:id')
+
     .get(userController.getUserByEmployeeNumber)
     .patch(userController.updateUser)
     .delete(userController.deleteUser);
