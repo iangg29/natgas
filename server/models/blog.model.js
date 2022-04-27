1f89f8ba12cf3b7968c44988fecf134ce789b93e
Pablo Cesar Jimenez Villeda <65324745+CesarJimenezVilleda02@users.noreply.github.com>
2022-04-27 13:09:17 -0500

    blog scheduled to delete in 10 seconds

--- server/models/blog.model.js
+++ server/models/blog.model.js
@@ -1,7 +1,9 @@
 const db = require('../db/database');
 const Base = require('./base.model');
+const cron = require('node-cron');
+const AppError = require('../utils/appError');
 
-module.exports = class extends Base {
+module.exports = class Blog extends Base {
     static table = 'blogpost';
 
     constructor({ date, title, content, image }) {
@@ -16,6 +18,16 @@ module.exports = class extends Base {
     }
 
     async save() {
+        const tableName = this.tableName;
+        const previous = await db(tableName).where({
+            slug: this.slug,
+        });
+        if (previous.length > 0)
+            throw new AppError('Ya existe un blog con este nombre', 400);
+
+        if (new Date(this.date) < new Date())
+            throw new AppError('No se pueden crear blogs en el pasado', 400);
+
         const idBlogPost = await db
             .insert({
                 date: this.date,
@@ -25,6 +37,30 @@ module.exports = class extends Base {
                 slug: this.slug,
             })
             .into(this.tableName);
+
+        // CREATE A DELETE DATE IN THE FUTURE
+        const deleteDate = new Date(
+            new Date(this.date).getDate() + 1000 * 60 * 60 * 24 * 30 * 2
+        );
+
+        // const cronExpr = `${deleteDate.getMinutes()} ${deleteDate.getHours()} ${deleteDate.getDate()} ${deleteDate.getMonth()} ${deleteDate.getDay()}`;
+        const cronExpr = '10 * * * * *';
+
+        // SET CRON
+        cron.schedule(
+            cronExpr,
+            async function () {
+                await db(tableName)
+                    .where({
+                        idBlogPost,
+                    })
+                    .del();
+
+                console.log('Destroyed Blog: ', idBlogPost);
+            },
+            false
+        );
+
         return db.select('*').from(this.tableName).where({
             idBlogPost,
         });
