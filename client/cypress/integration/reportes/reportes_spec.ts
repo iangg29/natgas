a68d4b7b9395bfc552327614e3011fad89d727c3
Ian García <iangarciag@outlook.com>
2022-04-27 22:26:53 -0500

    Implemented CU10 test.
    Component needed to be modified to enable cypress support.

---
+++ client/cypress/integration/reportes/reportes_spec.ts
@@ -0,0 +1,25 @@
+/// <reference types="cypress" />
+
+describe("Reports functionality.", (): void => {
+  beforeEach(() => {
+    // @ts-ignore
+    cy.login();
+    cy.visit(`${Cypress.env("testing_url")}/app/reports`);
+  });
+  afterEach(() => {
+    // @ts-ignore
+    cy.restoreLocalStorage();
+  });
+
+  it("[CU10] Registers new monthly report.", () => {
+    cy.contains("Reportes").should("be.visible");
+    cy.contains("Agregar reporte").should("exist");
+    cy.contains("Agregar reporte").click();
+    cy.get("input:first").type("{backspace}150");
+    cy.get("[id$=-add-btn]").within(() => {
+      cy.contains("Agregar").click();
+    });
+  });
+});
+
+export {};
