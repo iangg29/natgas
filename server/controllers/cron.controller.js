0aa5897ca8d866b3f655e986245829110487ca91
Pablo Cesar Jimenez Villeda <65324745+CesarJimenezVilleda02@users.noreply.github.com>
2022-04-28 10:04:31 -0500

    cron to update users ngb

--- server/controllers/cron.controller.js
+++ server/controllers/cron.controller.js
@@ -1,4 +1,6 @@
 const cron = require('node-cron');
+const Blog = require('../models/blog.model');
+const User = require('../models/user.model');
 
 exports.createBlogDeleteTask = (idBlogPost, date) => {
     // CREATE A DELETE DATE IN THE FUTURE
@@ -6,20 +8,26 @@ exports.createBlogDeleteTask = (idBlogPost, date) => {
         new Date(date).getDate() + 1000 * 60 * 60 * 24 * 30 * 2
     );
 
-    const cronExpr = `${deleteDate.getMinutes()} ${deleteDate.getHours()} ${deleteDate.getDate()} ${deleteDate.getMonth()} ${deleteDate.getDay()}`;
+    // const cronExpr = `${deleteDate.getMinutes()} ${deleteDate.getHours()} ${deleteDate.getDate()} ${deleteDate.getMonth()} ${deleteDate.getDay()}`;
+    const cronExpr = `10 * * * * *`;
 
     // SET CRON
     cron.schedule(
         cronExpr,
         async function () {
-            await db('blogpost')
-                .where({
-                    idBlogPost,
-                })
-                .del();
+            await Blog.deleteOne('idBlogPost', idBlogPost);
 
             console.log('Destroyed Blog: ', idBlogPost);
         },
         false
     );
+};
+
+exports.createUserNGBUpdateTask = () => {
+    const cronExpr = '0 0 1 1 *';
+
+    cron.schedule(cronExpr, async function () {
+        User.updateAll({ ngBlocks: 5 });
+        console.log('All users NGB requests avalilable set to 5');
+    });
 };
