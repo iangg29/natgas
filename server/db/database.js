c5380c7415e446f2a94ab4ef2fd2f4d3d5b37d94
MandukoGR <gurom84@outlook.com>
2022-05-06 18:51:16 -0500

    Minor Fixes

--- server/db/database.js
+++ server/db/database.js
@@ -11,7 +11,7 @@ if (process.env.NODE_ENV === 'production') {
             user: process.env.DB_USERNAME,
             password: process.env.DB_PASSWORD,
             database: process.env.DB_DATABASE,
-            host: process.env.DB_HOST,
+            host: process.env.INSTANCE_UNIX_SOCKET,
 
         },
     });
