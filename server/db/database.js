652242f8133db297ca61304dacf78d07c7f071d6
MandukoGR <gurom84@outlook.com>
2022-05-06 21:25:51 -0500

    Test

--- server/db/database.js
+++ server/db/database.js
@@ -4,6 +4,7 @@ attachPaginate();
 
 /* Creating a connection to the database. */
 
+if(process.env.NODE_ENV === "production"){
     const db = knex({
         client: 'mysql',
         connection: {
@@ -11,10 +12,29 @@ attachPaginate();
             password: process.env.DB_PASSWORD,
             database: process.env.DB_DATABASE,
             socketPath: '/cloudsql/smart-code-itesm:us-central1:natgas-db',
+            
 
         },
     });
     module.exports = db;
+
+    
+}else{
+    const db = knex({
+        client: 'mysql',
+        connection: {
+            user: process.env.DB_USERNAME,
+            port: process.env.DB_PORT,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_DATABASE,
+            host: process.env.DB_HOST,
+            
+
+        },
+    });
+    module.exports = db;
+}
+    
 
 
 
