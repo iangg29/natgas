d992aaf6b9cef49606f9a8f5eb4fc483c901d4c5
MandukoGR <gurom84@outlook.com>
2022-05-06 19:14:11 -0500

    Test

--- server/db/database.js
+++ server/db/database.js
@@ -8,10 +8,9 @@ attachPaginate();
         client: 'mysql',
         connection: {
             user: process.env.DB_USERNAME,
-            port: process.env.DB_PORT,
             password: process.env.DB_PASSWORD,
             database: process.env.DB_DATABASE,
-            host: process.env.DB_HOST,
+            socketPath: '/cloudsql/smart-code-itesm:us-central1:natgas-db',
 
         },
     });
