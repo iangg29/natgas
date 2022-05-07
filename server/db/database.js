2b2d30ea228fffc7bf110d7d3321e4109cdc4997
MandukoGR <gurom84@outlook.com>
2022-05-06 21:32:44 -0500

    Test

--- server/db/database.js
+++ server/db/database.js
@@ -4,38 +4,18 @@ attachPaginate();
 
 /* Creating a connection to the database. */
 
-if(process.env.NODE_ENV === "production"){
-    const db = knex({
-        client: 'mysql',
-        connection: {
-            user: process.env.DB_USERNAME,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_DATABASE,
-            socketPath: '/cloudsql/smart-code-itesm:us-central1:natgas-db',
-            
+const db = knex({
+    client: 'mysql',
+    connection: {
+        user: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+        socketPath: '/cloudsql/smart-code-itesm:us-central1:natgas-db',
+        
 
-        },
-    });
-    module.exports = db;
-
-    
-}else{
-    const db = knex({
-        client: 'mysql',
-        connection: {
-            user: process.env.DB_USERNAME,
-            port: process.env.DB_PORT,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_DATABASE,
-            host: process.env.DB_HOST,
-            
-
-        },
-    });
-    module.exports = db;
-}
-    
-
+    },
+});
+module.exports = db;
 
 
 
