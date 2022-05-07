7c9be4deb1f3736f0bee1bc5e6a82802f884ac6d
MandukoGR <gurom84@outlook.com>
2022-05-06 19:07:19 -0500

    Quedó como estaba lol

--- server/db/database.js
+++ server/db/database.js
@@ -3,36 +3,22 @@ const { attachPaginate } = require('knex-paginate');
 attachPaginate();
 
 /* Creating a connection to the database. */
-if (process.env.NODE_ENV === 'production') {
-    const dbSocketPath = '/cloudsql';
+
     const db = knex({
         client: 'mysql',
         connection: {
             user: process.env.DB_USERNAME,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_DATABASE,
-            host: process.env.INSTANCE_UNIX_SOCKET,
-
-        },
-    });
-    module.exports = db;
-}
-
-else{
-    const db = knex({
-        client: 'mysql',
-        connection: {
-            host: process.env.DB_HOST,
             port: process.env.DB_PORT,
-            user: process.env.DB_USERNAME,
             password: process.env.DB_PASSWORD,
             database: process.env.DB_DATABASE,
+            host: process.env.DB_HOST,
 
         },
     });
     module.exports = db;
 
-}
+
+
 
 
 
